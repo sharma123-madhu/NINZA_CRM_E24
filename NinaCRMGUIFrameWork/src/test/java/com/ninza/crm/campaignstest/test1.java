5321ae3053573fdff71aa78ad002e55d35be9f80
DELL <DELL@DESKTOP-N62URNS.lan>
2025-09-16 17:10:47 +0530

    ok

--- NinaCRMGUIFrameWork/src/test/java/com/ninza/crm/campaignstest/test1.java
+++ NinaCRMGUIFrameWork/src/test/java/com/ninza/crm/campaignstest/test1.java
@@ -1,10 +1,10 @@
 package com.ninza.crm.campaignstest;
 
-public class demo1 {
+public class test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     System.out.println("hi");
+
 	}
 
 }
