2fe0ff5af598a004e7d46d6e34a1207ddf14533d
DELL <DELL@DESKTOP-N62URNS.lan>
2025-09-19 00:27:18 +0530

    test edit

--- NinaCRMGUIFrameWork/src/main/java/com/ninzacrm/listenerutility/ListenerImplementation.java
+++ NinaCRMGUIFrameWork/src/main/java/com/ninzacrm/listenerutility/ListenerImplementation.java
@@ -1,20 +1,12 @@
 package com.ninzacrm.listenerutility;
 
-import java.io.File;
-import java.util.Date;
-
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.io.FileHandler;
-import org.testng.ISuite;
-import org.testng.ISuiteListener;
-import org.testng.ITestListener;
-import org.testng.ITestResult;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 
+import http://java.util.Date;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.io.FileHandler;
@@ -23,53 +15,68 @@ import org.testng.ISuiteListener;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-import com.ninza.crmbaseclass.BaseClass;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import com.ninza.crm.baseclass.BaseClass;
 
 public class ListenerImplementation implements ITestListener,ISuiteListener{
 
+	ExtentReports report;
+	ExtentTest test;
+	
 	@Override
 	public void onStart(ISuite suite) {
 		System.out.println("Report Configuration");
+		Date d=new Date();
+		String date = d.toString().replace(" ", "_").replace(":", "_");
+		ExtentSparkReporter spark=new ExtentSparkReporter("./ExtentReports/Report_"+date+".html");
+		spark.config().setDocumentTitle("CRM Report");
+		spark.config().setReportName("Ninza CRM Report");
+		spark.config().setTheme(Theme.DARK);
+		
+		report=new ExtentReports();
+		report.attachReporter(spark);
+		report.setSystemInfo("Browser", "Edge");
+		report.setSystemInfo("OS","Windows 10");
 	}
 
 	@Override
 	public void onFinish(ISuite suite) {
 		System.out.println("Report Backup");
+		report.flush();
 	}
 
 	@Override
 	public void onTestStart(ITestResult result) {
 		String methodName = result.getMethod().getMethodName();
-		System.out.println(methodName+" Execution started");
+		test = report.createTest(methodName);
+		test.log(http://Status.INFO, methodName+" Execution started");
 	}
 
 	@Override
 	public void onTestSuccess(ITestResult result) {
 		String methodName = result.getMethod().getMethodName();
-		System.out.println(methodName+" Execution success");
+		test.log(Status.PASS,methodName+" Execution success");
 	}
 
 	@Override
 	public void onTestFailure(ITestResult result) {
 		String methodName = result.getMethod().getMethodName();
-		System.out.println(methodName+" Execution Failed");
+		test.log(http://Status.FAIL, methodName+" Execution Failed");
 		Date d=new Date();
 		String date = d.toString().replace(" ", "_").replace(":", "_");
 		TakesScreenshot ts=(TakesScreenshot)BaseClass.sdriver;
-		File src=ts.getScreenshotAs(OutputType.FILE);
-		File dest=new File("./Screenshots/"+methodName+date+".png");
-		try {
-			FileHandler.copy(src, dest);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		String src=ts.getScreenshotAs(OutputType.BASE64);
+		test.addScreenCaptureFromBase64String(src, methodName+"_"+date);
 	}
 
 	@Override
 	public void onTestSkipped(ITestResult result) {
 		String methodName = result.getMethod().getMethodName();
-		System.out.println(methodName+" Execution skipped");
+		test.log(Status.SKIP, methodName+" Execution Failed");
 	}
 
 }
---
+++ NinaCRMGUIFrameWork/src/test/java/com/ninza/crm/campaignstest/test.java
@@ -0,0 +1,11 @@
+package com.ninza.crm.campaignstest;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubsyso
+		System.out.println("hi test");
+
+	}
+
+}
