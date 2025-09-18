187a8d69582d16731622748c75c45df873b702af
DELL <DELL@DESKTOP-N62URNS.lan>
2025-09-19 00:51:25 +0530

    edit the demo

---
+++ NinaCRMGUIFrameWork/src/test/java/com/ninza/crm/producttest/demo2.java
@@ -0,0 +1,10 @@
+package com.ninza.crm.producttest;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("hi  test demo");
+	}
+
+}
